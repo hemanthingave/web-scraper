aaf25d64efc1808a3c74f832b82d70f4dff40d08
Hemant Hingave <hemant.hingave@gmail.com>
2016-09-30 23:23:58 +0530

    web-scarper
    web-scarper

--- cfg.php
+++ cfg.php
@@ -1,16 +1,8 @@
 <?php	
 	//date_default_timezone_set('Asia/Calcutta');
-	//define('ADMIN_URL', 'sd');
 	set_time_limit(0); //no limit
-	if($_SERVER['SERVER_NAME']=='localhost'){
-		$conn = new mysqli('localhost', 'root','','hemant_dte');
-	} else {
-		$conn = new mysqli('localhost', 'dailyjoninfo','hemant@123','dailyjobinfo');
-	}
-	
+	$conn = new mysqli('localhost', 'root','','database_name');
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	} 
-	
-	
 ?>
